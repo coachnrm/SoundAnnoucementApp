d70493a71444988bfaee2e8ff9998b9573a88902
home <nuttapong_coach@hotmail.com>
2024-11-18 20:29:25 +0700

    add code

--- wwwroot/audio.js
+++ wwwroot/audio.js
@@ -1,7 +1,16 @@
+// window.audioHelper = {
+//     announceNumber: function (number) {
+//         const utterance = new SpeechSynthesisUtterance(`ขอเชิญคิวซักประวัติที่ ${number} ที่ช่องบริการ 2 ค่ะ`);
+//         utterance.lang = 'th-TH'; // Set the language to Thai
+//         window.speechSynthesis.speak(utterance);
+//     }
+// };
+
 window.audioHelper = {
-    announceNumber: function (number) {
-        const utterance = new SpeechSynthesisUtterance(`ขอเชิญคิวซักประวัติที่ ${number} ที่ช่องบริการ 2 ค่ะ`);
+    announceQueue: function (number, name, channel) {
+        const utterance = new SpeechSynthesisUtterance(`ขอเชิญคิวซักประวัติที่ ${number} คุณ${name} ที่${channel} ค่ะ`);
         utterance.lang = 'th-TH'; // Set the language to Thai
         window.speechSynthesis.speak(utterance);
     }
 };
+
