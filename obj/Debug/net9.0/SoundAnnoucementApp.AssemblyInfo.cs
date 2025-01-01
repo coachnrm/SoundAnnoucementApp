0cb24d529600327ac06ad862443c8cb9dab1252d
home <nuttapong_coach@hotmail.com>
2025-01-01 12:47:53 +0700

    add thai calendar

--- obj/Debug/net9.0/SoundAnnoucementApp.AssemblyInfo.cs
+++ obj/Debug/net9.0/SoundAnnoucementApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SoundAnnoucementApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bbab8dc7abd011f5fe7e8bde465a2fe958223e5f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2ffd2c8d9bec84197f4ddf4e7442adbb6e21e320")]
 [assembly: System.Reflection.AssemblyProductAttribute("SoundAnnoucementApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SoundAnnoucementApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
