150934226fc7c472debf11bf640c4bf92be40e7c
home <nuttapong_coach@hotmail.com>
2025-01-05 19:26:35 +0700

    add carousel

--- obj/Debug/net9.0/SoundAnnoucementApp.AssemblyInfo.cs
+++ obj/Debug/net9.0/SoundAnnoucementApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SoundAnnoucementApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d994c19b1d09defb0ad8e8672d72868edbb46318")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+02f3e78128b1d07ae9b826b541b910f4f090ca25")]
 [assembly: System.Reflection.AssemblyProductAttribute("SoundAnnoucementApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SoundAnnoucementApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
