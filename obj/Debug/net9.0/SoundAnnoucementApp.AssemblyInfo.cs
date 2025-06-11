951c98bd51a62daa520c7b6b08eb1670d9bbc957
toeitoei18 <hs4toei2544@gmail.com>
2025-06-11 13:01:58 +0700

    add video

--- obj/Debug/net9.0/SoundAnnoucementApp.AssemblyInfo.cs
+++ obj/Debug/net9.0/SoundAnnoucementApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SoundAnnoucementApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+14738dc9f43ac0d9f3defc8ff83dccbab64bcf29")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+74c52fae66d0b7ab64a21490dedacf375550f488")]
 [assembly: System.Reflection.AssemblyProductAttribute("SoundAnnoucementApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SoundAnnoucementApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
