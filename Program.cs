457899cf04aa052324e8837190670bf9cdd3fb77
toeitoei18 <hs4toei2544@gmail.com>
2025-06-16 10:53:15 +0700

    add code

--- Program.cs
+++ Program.cs
@@ -10,12 +10,19 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 //builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 // project จาก github https://github.com/Ph00n102/hosxpapi.git
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("http://localhost:5094/") });
+// builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("http://localhost:5094/") });
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("http://172.16.200.202:8089/") });
 
 // project จาก github https://github.com/coachnrm/OpNoteImageApi.git
 builder.Services.AddHttpClient("OpNoteApi", client =>
 {
     client.BaseAddress = new Uri("http://localhost:5286/");
+});
+
+builder.Services.AddHttpClient("BackEnd", client =>
+{
+    client.BaseAddress = new Uri("http://172.16.200.202:8089/");
+    // client.BaseAddress = new Uri("http://localhost:5094/");
 });
 
 builder.Services.AddSingleton<QueueService>();
