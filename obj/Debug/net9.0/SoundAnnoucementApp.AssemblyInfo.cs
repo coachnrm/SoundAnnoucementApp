cac3a209df6862c365f6ebfa4254854cd5750f0d
coachnrm <nuttapong_coach@hotmail.com>
2025-08-06 14:46:35 +0700

    add code

--- obj/Debug/net9.0/SoundAnnoucementApp.AssemblyInfo.cs
+++ obj/Debug/net9.0/SoundAnnoucementApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SoundAnnoucementApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aca10c8388d78632738ae7d340f7e1be61c50698")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0798ce7c71fbafd6d184ee55fc7f8912b92d36e2")]
 [assembly: System.Reflection.AssemblyProductAttribute("SoundAnnoucementApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SoundAnnoucementApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
